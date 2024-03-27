6bf701894234eb16cf550067fb4e0b5a256d4318
WHB <1992756865@qq.com>
2024-03-27 10:26:10 +0800

    新增博客源代码 vite+vue-ts【vue3】 + vitePress

---
+++ blog-now/docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'WHB',
+  description: 'Whb Blog',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '博客', link: '' },
+      { text: '前端技术', link: '' },
+      { text: '后端技术', link: '' },
+      { text: '中医', link: '' },
+      { text: 'DIY', link: '' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/WHB1' }],
+  },
+})
---
+++ blog-now/docs/index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: '个人技术梳理录'
+  text: ''
+  tagline: 纸上得来终觉浅，绝知此事要躬行。
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Vue
+    details: Vue 技术 以及 源码解析
+  - title: JavaScript
+    details: 面向对象编程、数据结构与算法
+  - title: WebGL
+    details: canvas、3D、数据统计、gis
+  - title: VitePress & GitHub Pages 个人博客
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 中医
+    details: 基础理论、针灸、推拿、养生
+---
